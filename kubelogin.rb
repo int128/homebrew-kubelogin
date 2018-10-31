7d15c5ffc8aa5fa8316f16b4f5b2ebb08c0f456e
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-31 14:23:06 +0900

    Brew formula update for kubelogin version 1.8

--- kubelogin.rb
+++ kubelogin.rb
@@ -1,9 +1,9 @@
 class Kubelogin < Formula
   desc "kubectl with OpenID Connect (OIDC) authentication"
   homepage "https://github.com/int128/kubelogin"
-  url "https://github.com/int128/kubelogin/releases/download/1.7/kubelogin_1.7_darwin_amd64.tar.gz"
-  version "1.7"
-  sha256 "44954b8cb84ff9aa403c881eb97784559c8aa4803061ef3df76267cb1c4a2a5b"
+  url "https://github.com/int128/kubelogin/releases/download/1.8/kubelogin_1.8_darwin_amd64.tar.gz"
+  version "1.8"
+  sha256 "f0151ffd18effdc017a813ef669539c6fc25c5e0e1198db4378d1e09558dfa8d"
 
   def install
     bin.install "kubelogin"
