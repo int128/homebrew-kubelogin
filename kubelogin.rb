01ac5998a97cb45c78158736500feab793fb0980
Hidetake Iwata <int128@gmail.com>
2019-09-11 10:03:50 +0900

    v1.14.3

--- kubelogin.rb
+++ kubelogin.rb
@@ -1,9 +1,9 @@
 class Kubelogin < Formula
   desc "A kubectl plugin for Kubernetes OpenID Connect authentication"
   homepage "https://github.com/int128/kubelogin"
-  url "https://github.com/int128/kubelogin/releases/download/v1.14.2/kubelogin_darwin_amd64.zip"
-  version "v1.14.2"
-  sha256 "061481eb5650555a874d12abea3ae9e69d3a0bb37249d275dcfb6dc9f76029ce"
+  url "https://github.com/int128/kubelogin/releases/download/v1.14.3/kubelogin_darwin_amd64.zip"
+  version "v1.14.3"
+  sha256 "6672bcb9e1138b8eb72e13f6f6ee4f2cbcea1d8f6b94c4767e8272e4565839cf"
   def install
     bin.install "kubelogin" => "kubelogin"
     ln_s bin/"kubelogin", bin/"kubectl-oidc_login"
