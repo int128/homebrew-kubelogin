7c9b228f0b150ae1499ec2b99ed7c909d534822a
Hidetake Iwata <int128@gmail.com>
2020-05-16 18:16:21 +0900

    Bump the version to v1.19.1 (#8)
    * Bump the version to v1.19.1
    
    * Add .circleci/config.yml

--- kubelogin.rb
+++ kubelogin.rb
@@ -1,15 +1,27 @@
 class Kubelogin < Formula
   desc "A kubectl plugin for Kubernetes OpenID Connect authentication"
   homepage "https://github.com/int128/kubelogin"
-  url "https://github.com/int128/kubelogin/releases/download/v1.19.0/kubelogin_darwin_amd64.zip"
-  version "v1.19.0"
-  sha256 "1f379d57a14024995ba7ec5d2ce4f5aba8448c117189e9f505cf22cf85a01638"
+  baseurl = "https://github.com/int128/kubelogin/releases/download"
+  version "v1.19.1"
+  
+  if OS.mac?
+    kernel = "darwin"
+    sha256 "51806d291e69cd4dce54af7bb03adf76a55397741ea00fee4d845718a171ddc0"
+  elsif OS.linux?
+    kernel = "linux"
+    sha256 "3bfe06b8d1935f4276335c7c7d08945f5fcb5735b9d74701663f88908e7edef7"
+  end 
+   
+  url baseurl + "/#{version}/kubelogin_#{kernel}_amd64.zip"
+    
   def install
     bin.install "kubelogin" => "kubelogin"
     ln_s bin/"kubelogin", bin/"kubectl-oidc_login"
   end
+  
   test do
     system "#{bin}/kubelogin -h"
     system "#{bin}/kubectl-oidc_login -h"
   end
+    
 end
