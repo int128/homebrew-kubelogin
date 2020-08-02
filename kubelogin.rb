261e61025540d3fc73d634e0f167eed1cad5732b
Hidetake Iwata <int128@gmail.com>
2020-08-02 15:24:15 +0900

    Bump the version to v1.20.0 (#14)

--- kubelogin.rb
+++ kubelogin.rb
@@ -2,14 +2,14 @@ class Kubelogin < Formula
   desc "A kubectl plugin for Kubernetes OpenID Connect authentication"
   homepage "https://github.com/int128/kubelogin"
   baseurl = "https://github.com/int128/kubelogin/releases/download"
-  version "v1.19.4"
+  version "v1.20.0"
   
   if OS.mac?
     kernel = "darwin"
-    sha256 "1b23f53f45639c451093d47ba130882b7de8de5d5c22634e35dd7fbaa4f8a766"
+    sha256 "1ce071a80aa8aa6cd2c80a6c900e109d1a7b451907952854330bc252e4196bf3"
   elsif OS.linux?
     kernel = "linux"
-    sha256 "8273b6426d8f29e357000b9e7e7c70c30d40d9c343e21408a32be612b15a69eb"
+    sha256 "c0063f8060e969fa8c8fcfb4e37f2bbe35e7537cdbc82dd0535c96098953c6a6"
   end 
    
   url baseurl + "/#{version}/kubelogin_#{kernel}_amd64.zip"
