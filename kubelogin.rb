3b5074a9141833d0a7cfefa7a62a1925d2fe1ff9
Hidetake Iwata <int128@gmail.com>
2021-02-11 19:21:51 +0900

    Update kubelogin.rb (#20)

--- kubelogin.rb
+++ kubelogin.rb
@@ -1,18 +1,16 @@
 class Kubelogin < Formula
   desc "A kubectl plugin for Kubernetes OpenID Connect authentication"
   homepage "https://github.com/int128/kubelogin"
-  baseurl = "https://github.com/int128/kubelogin/releases/download"
   version "v1.22.0"
   
-  if OS.mac?
-    kernel = "darwin"
+  on_macos do
+    url "https://github.com/int128/kubelogin/releases/download/v1.22.0/kubelogin_darwin_amd64.zip"
     sha256 "b87b4ed009c05e93a0ed529dc7e9396a0357b207e375b1b42f12edb04df13bba"
-  elsif OS.linux?
-    kernel = "linux"
+  end
+  on_linux do
+    url "https://github.com/int128/kubelogin/releases/download/v1.22.0/kubelogin_linux_amd64.zip"
     sha256 "4ce6e6d2f57e2b245bbd7700bd425dd655bae488dc995524a1e70ceda27e5ff9"
-  end 
-   
-  url baseurl + "/#{version}/kubelogin_#{kernel}_amd64.zip"
+  end
     
   def install
     bin.install "kubelogin" => "kubelogin"
