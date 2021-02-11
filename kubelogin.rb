5ae0d68e22b93158fca503f1f7fa25e34ca7e49d
int128bot <78065832+int128bot@users.noreply.github.com>
2021-02-11 21:23:44 +0900

    Bump the version to v1.22.1

--- kubelogin.rb
+++ kubelogin.rb
@@ -1,25 +1,24 @@
 class Kubelogin < Formula
   desc "A kubectl plugin for Kubernetes OpenID Connect authentication"
   homepage "https://github.com/int128/kubelogin"
-  version "v1.22.0"
-  
+  version "v1.22.1"
+
   on_macos do
-    url "https://github.com/int128/kubelogin/releases/download/v1.22.0/kubelogin_darwin_amd64.zip"
-    sha256 "b87b4ed009c05e93a0ed529dc7e9396a0357b207e375b1b42f12edb04df13bba"
+    url "https://github.com/int128/kubelogin/releases/download/v1.22.1/kubelogin_darwin_amd64.zip"
+    sha256 "c6c5ce4f707863821a8c3f2b4fc4e982a8be05455331f54099727d6439f83c99"
   end
   on_linux do
-    url "https://github.com/int128/kubelogin/releases/download/v1.22.0/kubelogin_linux_amd64.zip"
-    sha256 "4ce6e6d2f57e2b245bbd7700bd425dd655bae488dc995524a1e70ceda27e5ff9"
+    url "https://github.com/int128/kubelogin/releases/download/v1.22.1/kubelogin_linux_amd64.zip"
+    sha256 "dfbc8a6535178f58c2d96bd8d1061d4688de8c83eb1cae9d0db687224ebc6726"
   end
-    
+
   def install
     bin.install "kubelogin" => "kubelogin"
     ln_s bin/"kubelogin", bin/"kubectl-oidc_login"
   end
-  
+
   test do
     system "#{bin}/kubelogin -h"
     system "#{bin}/kubectl-oidc_login -h"
   end
-    
 end
