0eff1664ccea745dbfe4ebb46cc282e7fc4ae8b4
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2022-07-18 04:21:17 +0000

    Fix consistency of generated files

--- oidc-kubelogin.rb
+++ oidc-kubelogin.rb
@@ -6,16 +6,16 @@ class OidcKubelogin < Formula
 
   case
   when OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/int128/kubelogin/releases/download/v#{version}/kubelogin_darwin_amd64.zip"
+    url "https://github.com/int128/kubelogin/releases/download/v1.25.1/kubelogin_darwin_amd64.zip"
     sha256 "7bb080e2bd3928ae0cd275baede6e1fcc4a1e8252d15598254cda609cb0095f3"
   when OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/int128/kubelogin/releases/download/v#{version}/kubelogin_darwin_arm64.zip"
+    url "https://github.com/int128/kubelogin/releases/download/v1.25.1/kubelogin_darwin_arm64.zip"
     sha256 "1c3c4bec277e31fd3c9348495d769c89175e210cd6507e54c873726bf6070d0c"
   when OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/int128/kubelogin/releases/download/v#{version}/kubelogin_linux_amd64.zip"
+    url "https://github.com/int128/kubelogin/releases/download/v1.25.1/kubelogin_linux_amd64.zip"
     sha256 "658499b338b8cee7c622130d68405a55b4c4051ba43f7c841cb3f3d9364b3292"
   when OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/int128/kubelogin/releases/download/v#{version}/kubelogin_linux_arm64.zip"
+    url "https://github.com/int128/kubelogin/releases/download/v1.25.1/kubelogin_linux_arm64.zip"
     sha256 "45cc255a494296bc8d3ed453e7d6e35ebe2e1a58e2369ba1d15fddecc3c29630"
   else
     odie "Unexpected platform!"
